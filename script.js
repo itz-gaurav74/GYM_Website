f4c9f2d9963683aa39920acd43a58329f714dcfd
Gaurav <4gaurav07@gmail.com>
2023-09-25 19:19:42 +0530

    Mobile View Added

--- script.js
+++ script.js
@@ -5,30 +5,6 @@ var typed = new Typed('#typed-text',{
     loop : true
 });
 
-
-
-// Scroll to specific values
-// scrollTo is the same
-// window.scroll({
-//     top: 2500, 
-//     left: 0, 
-//     behavior: 'smooth'
-//   });
-  
-//   // Scroll certain amounts from current position 
-//   window.scrollBy({ 
-//     top: 100, // could be negative value
-//     left: 0, 
-//     behavior: 'smooth' 
-//   });
-  
-//   // Scroll to a certain element
-//   document.querySelector('.body').scrollIntoView({ 
-//     behavior: 'smooth' 
-//   });
-
-
-
   // Scroll To Top
   let scrollBtn=document.getElementsByClassName("scrollToTop")[0];
   scrollBtn.addEventListener("click",()=>{
